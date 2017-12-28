42c7806a2b71ec0d1ae2e12dfab24a0eab0bc18a
Chris Grabosky <chris@grabosky.net>
2017-12-28 22:04:18 +0000

    QSVenv.sh edited online with Bitbucket

--- QSVenv.sh
+++ QSVenv.sh
@@ -2,30 +2,6 @@
 
 venvDir="pyVenv"`date +%s`
 path="/mnt/c/Users/chris/Desktop"
-
-cd  ${path}
-
-python -m virtualenv ${path}/${venvDir}
-
-echo "cloudshell-shell-core>=2.4.0
-cloudshell-networking>=4.0.0,<4.1.0
-cloudshell-networking-devices>=2.2,<2.3
-cloudshell-snmp
-cloudshell_automation_api
-cloudshell-cli
-requests
-shellfoundry" > ${path}/${venvDir}/requirements.txt
-
-${path}/${venvDir}/bin/pip install -r ${path}/${venvDir}/requirements.txt
-
-echo 
-echo "venv set up in: " ${venvDir}
-echo
-
-echo "configuring shellfoundry"#!/bin/bash
-
-venvDir="pyVenv"`date +%s`
-path="/mnt/c/Users/chris/Desktop"
 if [[ `pwd` == *"Desktop"* ]] 
     then 
     path = `pwd`
@@ -52,13 +28,6 @@ echo "venv set up in: " ${venvDir}
 echo
 
 echo "configuring shellfoundry"
-${path}/${venvDir}/bin/shellfoundry config host 192.168.2.251
-${path}/${venvDir}/bin/shellfoundry config 
-
-echo 
-echo
-echo "DONE!"
-echo
 ${path}/${venvDir}/bin/shellfoundry config host 192.168.2.251
 ${path}/${venvDir}/bin/shellfoundry config 
 
