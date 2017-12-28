24da621fb3723f0a4275c4e32ef9a8fc43029bc4
Chris Grabosky <chris@grabosky.net>
2017-12-28 21:40:58 +0000

    QSVenv.sh created online with Bitbucket

---
+++ QSVenv.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+venvDir="pyVenv"`date +%s`
+path="/mnt/c/Users/chris/Desktop"
+
+cd  ${path}
+
+python -m virtualenv ${path}/${venvDir}
+
+echo "cloudshell-shell-core>=2.4.0
+cloudshell-networking>=4.0.0,<4.1.0
+cloudshell-networking-devices>=2.2,<2.3
+cloudshell-snmp
+cloudshell_automation_api
+cloudshell-cli
+requests
+shellfoundry" > ${path}/${venvDir}/requirements.txt
+
+${path}/${venvDir}/bin/pip install -r ${path}/${venvDir}/requirements.txt
+
+echo 
+echo "venv set up in: " ${venvDir}
+echo
+
+echo "configuring shellfoundry"
+${path}/${venvDir}/bin/shellfoundry config host 192.168.2.251
+${path}/${venvDir}/bin/shellfoundry config 
+
+echo 
+echo
+echo "DONE!"
+echo
