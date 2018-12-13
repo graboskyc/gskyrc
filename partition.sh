78f29f163bedbc67db696c62b3c72151653a02c3
Chris Grabosky <chris@grabosky.net>
2018-12-13 19:00:21 +0000

    partition.sh edited online with Bitbucket

--- partition.sh
+++ partition.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-randArrayElement(){ arr=("${!1}"); echo ${arr["$[RANDOM % ${#arr[@]}]"]}; }
-foo=("|" "\\" "/" "-")
-randArrayElement "foo[@]"
+foo=("/" "-" "\\" "|")
+s=`date "+%s"`
+let m="s%4"
+echo ${foo[m]}
