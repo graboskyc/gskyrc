3f5cf7e883adc7bcc1cac7c3ee20289ae0536b8b
Chris Grabosky <chris@grabosky.net>
2019-02-06 19:46:00 +0000

    mac vs linux support

--- mem.sh
+++ mem.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+# UNCOMMENT BELOW FOR MAC
 ram=`sysctl -n hw.memsize`
 let ram="ram/1024/1024/1024"
 used=`top -i1 -l1 | grep "Phy" | cut -d" " -f 2`
-echo "RAM Used: ${used}B/${ram}GB"
+echo "RAM Used: ${used}GB/${ram}GB"
+
+# UNCOMMENT BELOW FOR LINUX
+#ram=` free -m | head -n 2 | tail -n 1 | cut -d":" -f 2 | sed --expression='s/ \+/ /g' | cut -d" " -f 2`
+#used=` free -m | head -n 2 | tail -n 1 | cut -d":" -f 2 | sed --expression='s/ \+/ /g' | cut -d" " -f 3`
+#echo "RAM Used: ${used}MB/${ram}MB"
